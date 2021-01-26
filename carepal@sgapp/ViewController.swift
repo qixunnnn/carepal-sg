d51879b33075c9d39331982457412b7788174cde
jw <wyipjun@gmail.com>
2021-01-27 06:03:45 +0800

    .

--- carepal@sgapp/ViewController.swift
+++ carepal@sgapp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("ur mother")
+        print("test ur mother")
     }
 
 
