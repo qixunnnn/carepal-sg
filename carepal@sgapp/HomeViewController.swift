eb2ddb5cc35164acabf580b694e59cd20cdd96ca
Qi Xun <qixunnn@gmail.com>
2021-01-30 06:15:37 +0800

    Done with news API page. Trying to convert string to date and back to string

--- carepal@sgapp/HomeViewController.swift
+++ carepal@sgapp/HomeViewController.swift
@@ -17,7 +17,6 @@ class HomeViewController: UIViewController {
 //        let articles = Article(source: source, author: "Endofound.org", title: "Everything You Need to Know About Cervical Cancer - Endometriosis Foundation of America - The Blossom", description: "January is Cervical Health Awareness Month", url: "https://www.endofound.org/everything-you-need-to-know-about-cervical-cancer", urlToImage: "https://www.endofound.org/member_files/object_files/endofound.org/2021/01/28/january_is_cervical_health_awareness_month.png", publishedAt: 2021-01-28T16:46:33Z, content: "January is Cervical Health Awareness Month. This month, various organizations embark on campaigns geared towards increasing awareness and public education on cervical cancer in their communities, enc… [+5979 chars]")
         //let sample = News(status: "ok", totalResults: 70, articles:)
         getData { (News) in
-
         }
     }
     //    This is news
@@ -35,6 +34,9 @@ class HomeViewController: UIViewController {
             do
             {
                 let response = try JSONDecoder().decode(News.self, from: data)
+//                DispatchQueue.main.async {
+//
+//                }
                 completionHandler(response)
             }
             catch
