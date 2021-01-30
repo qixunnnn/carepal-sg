05491d8108247d1a32487346642a1b0a6f540319
jw <wyipjun@gmail.com>
2021-01-31 00:23:11 +0800

    map done

--- carepal@sgapp/MapsViewController.swift
+++ carepal@sgapp/MapsViewController.swift
@@ -20,16 +20,36 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
     let locationManager = CLLocationManager()
     let np = CLLocationCoordinate2D(latitude: 1.333498666, longitude: 103.772830242)
     let jp = CLLocationCoordinate2D(latitude: 1.3400319732, longitude: 103.704087184)
+    var newLocation = false
     var destination = CLLocationCoordinate2D.init()
     var tapped = false
     var location: CLLocation!
     public var currentCoordinates: CLLocationCoordinate2D!
-    var directionsArray: [MKDirections] = []
+    
+    let annoCoordinates: Array<CLLocationCoordinate2D> =
+        [CLLocationCoordinate2D(latitude: 1.3485, longitude: 103.7115),//boon lay cc,
+         CLLocationCoordinate2D(latitude: 1.3189, longitude: 103.7681),//clementi cc
+         CLLocationCoordinate2D(latitude: 1.3401, longitude: 103.7372)//clementi cc
+        ]
+    let annoTitle: Array<String> =
+        ["Boon lay CC",
+        "Clementi CC",
+        "Yuhua CC"]
+    let annoSubtitle: Array<String> =
+        ["10 Boon Lay Pl Singapore 649882",
+        "220 Clementi Ave 4 Singapore 129880",
+        "90 Boon Lay Way Singapore 609958"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         activityIndicator.hidesWhenStopped = true
         calculatingRLbl.isHidden = true
         overrideUserInterfaceStyle = .light
+   
+       
+        
+        //map
         mapView.delegate = self
         mapView.register(CustomAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         // Ask for Authorisation from the User.
@@ -45,17 +65,23 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .followWithHeading
         }
-       // let span: MKCoordinateSpan = MKCoordinateSpanMake(0.3, 0.3)
-            
-        //Zoom to user location
-//           if let userLocation = locationManager.location?.coordinate {
-//            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 1000, longitudinalMeters: 1000)
-//               mapView.setRegion(viewRegion, animated: false)
-//           }
-
-      
+        
+       
+        var c = 0
+        for i in annoCoordinates
+        {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = i
+            annotation.title = annoTitle[c]
+            annotation.subtitle = annoSubtitle[c]
+            mapView.addAnnotation(annotation)
+            c+=1
+        }
+    
+        
+        
+      /*
         let geocoder = CLGeocoder()
-        let annotation = MKPointAnnotation()
         geocoder.geocodeAddressString("535 Clementi Road Singapore 599489", completionHandler:
             {(p,e) in
                 let coordinates = CLLocationCoordinate2D(latitude: p![0].location!.coordinate.latitude, longitude: p![0].location!.coordinate.longitude)
@@ -65,32 +91,25 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
                 self.mapView.addAnnotation(annotation)
                 
             })
+ 
+        */
         
         
-       
-        // Do any additional setup after loading the view.
     }
     
+    //go to current location btn
     @IBAction func centerMapOnUserLocation(_ sender: Any) {
         guard let coordinate = locationManager.location?.coordinate else {return}
         let region = MKCoordinateRegion(center:coordinate, latitudinalMeters: 2000, longitudinalMeters: 2000)
+        mapView.userTrackingMode = .followWithHeading
         mapView.setRegion(region, animated: true)
     }
-    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!,fromLocation oldLocation: CLLocation!)
-    {
-       
-    }
+    
+  
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        //let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+      
+        mapView.mapType = MKMapType.standard
 
-//        mapView.mapType = MKMapType.standard
-//
-//
-//        let annotation = MKPointAnnotation()
-//        annotation.coordinate = locValue
-//        annotation.title = "Me"
-//        annotation.subtitle = "current location"
-//        mapView.addAnnotation(annotation)
         guard let currentlocation = locations.last else {return}
         location = currentlocation
         if(tapped)
@@ -101,7 +120,7 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
                
                 if previousLocation.distance(from: currentlocation) > 10 {
                    //show route
-                    
+                        mapView.userTrackingMode = .followWithHeading
                         showRouteOnMap(pickupCoordinate: currentlocation.coordinate, destinationCoordinate: destination)
                         
                     previousLocation = currentlocation
@@ -110,7 +129,7 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
             else{
                 //in case previous location doesn't exist
                 //show route
-               
+                mapView.userTrackingMode = .followWithHeading
                     showRouteOnMap(pickupCoordinate: currentlocation.coordinate, destinationCoordinate: destination)
                    
                 previousLocation = currentlocation
@@ -119,11 +138,10 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
         }
         
         
-        mapView.userTrackingMode = .followWithHeading
-        //centerMap(locValue)
+       
     }
   
-    
+    //to clear route
     func resetMapView(withNew directions:MKDirections)
     {
         mapView.removeOverlays(mapView.overlays)
@@ -132,25 +150,15 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         tapped = true
-        //destination = np
+        newLocation = true
         calculatingRLbl.isHidden = false
         activityIndicator.startAnimating()
+        self.destination = view.annotation!.coordinate
+        previousLocation = nil
         
-        let geoCoder = CLGeocoder()
-        geoCoder.geocodeAddressString((view.annotation?.subtitle)!!) { (placemarks, error) in
-            guard
-                let placemarks = placemarks,
-                let location = placemarks.first?.location
-                
-            else {
-                // handle no location found
-                return
-            }
-            self.destination = location.coordinate
-        }
-       // print(view.annotation?.title!!)
-        //print(view.annotation?.subtitle!!)
+
     }
+    //create route
     func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
 
             let request = MKDirections.Request()
@@ -176,13 +184,17 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate, MKMapView
                     
                     self.mapView.addOverlay(route.polyline)
                     //set the map area to show the route
-                    self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets.init(top: 80.0, left: 20.0, bottom: 100.0, right: 20.0), animated: true)
+                    if(newLocation)
+                    {
+                        self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets.init(top: 80.0, left: 20.0, bottom: 100.0, right: 20.0), animated: true)
+                        newLocation = false
+                    }
+                    
                     calculatingRLbl.isHidden = true
                     activityIndicator.stopAnimating()
                 }
 
-                //if you want to show multiple routes then you can get all routes in a loop in the following statement
-                //for route in unwrappedResponse.routes {}
+               
             }
         }
     //this delegate function is for displaying the route overlay and styling it
@@ -201,7 +213,7 @@ extension ViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation { return nil }
 
-        let reuseIdentifier = "..."
+        let reuseIdentifier = "."
 
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
 
