e2689154d0d9ffe032e8f566397fb1eb0ecae284
jw <wyipjun@gmail.com>
2021-01-31 22:27:27 +0800

    tableview rewards and redeem

--- carepal@sgapp/RewardViewController.swift
+++ carepal@sgapp/RewardViewController.swift
@@ -18,16 +18,35 @@ class RewardViewCell:UITableViewCell {
 }
 class RewardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
-    
+    let CellTitle: Array<String> =
+        ["PandaMart",
+         "NTUC Fairprice",
+         "GrabFood"//clementi cc
+        ]
+    let CellDetails: Array<String> =
+        ["100% off delivery using PandaMart",
+         "15% off NTUC products",
+         "100% off delivery using GrabFood"//clementi cc
+        ]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
 
         self.tableView.dataSource = self
         self.tableView.rowHeight = 103
+        overrideUserInterfaceStyle = .light
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear( _ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear( _ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     // MARK: - Table view data source
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -38,20 +57,17 @@ class RewardViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         //return fetchArticles.count
-        return 1//array count
+        return CellTitle.count//array count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RewardCell", for: indexPath) as! RewardViewCell
-        //let cNew = fetchArticles[indexPath.row] //Passing later
-        
-       // cell.titleText?.text = fetchArticles[indexPath.row].title
-        cell.TitleLbl.text = "PandaMart"
-        cell.DetailLbl.text = "100% off delivery using PandaMart"
-        cell.LogoImg.image = UIImage(named: "FoodPanda")
-//        cell.titleText.numberOfLines = 0
-//        cell.titleText.sizeToFit()
-      
+    
+        cell.TitleLbl.text = String(CellTitle[indexPath.row])
+        cell.DetailLbl.text = String(CellDetails[indexPath.row])
+        cell.LogoImg.image = UIImage(named: String(CellTitle[indexPath.row]))
+
+
         return cell
     }
     
